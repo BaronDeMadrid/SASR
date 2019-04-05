6aebf8f22ae2bf583ac92bdf98273f00091c3c89
q <q@q-VirtualBox>
2019-04-05 19:47:58 +0200

    XQUERY

--- Swing/src/Swing/MainSwing.java
+++ Swing/src/Swing/MainSwing.java
@@ -2,7 +2,7 @@ package Swing;
 import javax.swing.JFrame;
 public class MainSwing extends JFrame{
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		Swinger frame = new Swinger();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
